74543d4c010158f710696dd5dd19f1e0e901a863
benonymus <bence1204@gmail.com>
2018-09-10 00:06:37 +0700

    a

--- lib/userteam1/web/recording.ex
+++ lib/userteam1/web/recording.ex
@@ -1,9 +1,10 @@
 defmodule Userteam1.Web.Recording do
   use Ecto.Schema
+  use Arc.Ecto.Schema
   import Ecto.Changeset
 
   schema "recordings" do
-    field(:path_to_recording, :string)
+    field(:path_to_recording, Userteam1Web.Recording.Type)
     field(:mod_score, :integer)
     belongs_to(:user, Userteam1.Web.User)
     belongs_to(:challenge, Userteam1.Web.Challenge)
@@ -15,7 +16,8 @@ defmodule Userteam1.Web.Recording do
   @doc false
   def changeset(recording, attrs) do
     recording
-    |> cast(attrs, [:path_to_recording, :challenge_id, :user_id, :mod_score])
+    |> cast(attrs, [:challenge_id, :user_id, :mod_score])
+    |> cast_attachments(attrs, [:path_to_recording])
     |> validate_required([:path_to_recording])
   end
 end
