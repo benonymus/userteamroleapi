385be6d3f9b8cb5ec8878a2dd776bdc8790bda81
benonymus <bence1204@gmail.com>
2018-10-24 22:48:40 +0700

    a

--- lib/userteam1_web/controllers/challenge_group_controller.ex
+++ lib/userteam1_web/controllers/challenge_group_controller.ex
@@ -53,8 +53,12 @@ defmodule Userteam1Web.ChallengeGroupController do
 
   def delete(conn, %{"id" => id}) do
     challenge_group = Web.get_challenge_group!(id)
-    {:ok, path} = Userteam1Web.ChallengeGroupAvatar.store(challenge_group.avatar)
-    :ok = Userteam1Web.ChallengeGroupAvatar.delete(path)
+
+    if challenge_group.avatar do
+      {:ok, path} = Userteam1Web.ChallengeGroupAvatar.store(challenge_group.avatar)
+      :ok = Userteam1Web.ChallengeGroupAvatar.delete(path)
+    end
+
     {:ok, _challenge_group} = Web.delete_challenge_group(challenge_group)
 
     conn
