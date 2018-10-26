16f2ad3e45b69f5f23cf29adf9e562c7c56fa3ac
benonymus <bence1204@gmail.com>
2018-10-26 13:16:18 +0700

    a

--- lib/userteam1_web/controllers/recording_controller.ex
+++ lib/userteam1_web/controllers/recording_controller.ex
@@ -122,7 +122,8 @@ defmodule Userteam1Web.RecordingController do
       from(
         r in Rating,
         where: r.user_id == ^user.id,
-        select: sum(r.amount)
+        select: r
+        # select: sum(r.amount)
       )
 
     Repo.all(score_query)
